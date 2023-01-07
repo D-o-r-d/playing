a781931088a2fd9f6b7ffba3915eeb85122bce0e
D-o-r-d <121807202+D-o-r-d@users.noreply.github.com>
2023-01-07 17:12:45 +0900

    Add files via upload

---
+++ brainfuck.py
@@ -0,0 +1,29 @@
+moji = str(input("words:"))
+code = [""]
+now = 0
+j = 0
+
+for i in moji:
+    a = ord(i)
+    flag = 0
+    roop = -1
+    while flag == 0:
+        roop += 1
+        if abs(now - a) <= roop**2 + ((roop+1)**2 - roop**2 - 1 ) / 2:
+            flag = 1
+    surplus = abs(now - a) - roop**2
+    if (surplus < 0 and now < a) or (surplus > 0 and now > a):
+        fix = "-" * abs(surplus)
+    elif (surplus > 0 and now < a) or (surplus < 0 and now > a):
+        fix = "+" * abs(surplus)
+    else:
+        fix = ""
+
+    if a > now:
+        code[j] = str("+" * roop) + "[>" + str("+" * roop) + "<-]>" + str(fix) + ".<"
+    else:
+        code[j] = str("+" * roop) + "[>" + str("-" * roop) + "<-]>" + str(fix) + ".<"
+    now = a
+    code.append("")
+    print(code[j])
+    j += 1
